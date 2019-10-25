b630187ecbde5b17d3e0e98df716e4062549bc47
colin <cjsords@gmail.com>
2019-10-25 14:15:41 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+//clicking on div with corresponding id will run the changeColor() function that changes the color
+document.getElementById("change").onclick = changeColor;
+
+function changeColor() {
+  document.getElementById("change").style.backgroundColor = "blue";
+}
+
+//make paragraph dissapear
+document.getElementById("go").onclick = makeInvisible;
+
+function makeInvisible() {
+  document.getElementById("go").style.visibility = "hidden";
+}
+
+document.getElementById("enter").onmouseenter = changeText;
+function changeText() {
+  document.getElementById("enter").innerHTML = "Hi";
+}
+
+document.getElementById("enter").onmouseleave = changeTextBack;
+function changeTextBack() {
+  document.getElementById("enter").innerHTML = "Bye";
+}
+
+//https://www.youtube.com/watch?v=qvypCd2Vl4s
+document.getElementById("clock").onload = showTime();
+function showTime() {
+  let date = new Date();
+  let h = date.getHours();
+  let m = date.getMinutes();
+  let s = date.getSeconds();
+  let time = `${h}:${m}:${s}`;
+  document.getElementById("clock").innerHTML = time;
+  setTimeout(showTime, 1000);
+}
