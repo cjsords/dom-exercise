b6bfe04e2ce300242b94c06109ee51e3d6d33cef
Colin <cjsords@gmail.com>
2019-10-27 20:21:59 -0400

    refactored using queryselectors

--- script.js
+++ script.js
@@ -10,22 +10,39 @@ function changeColor() {
   square.classList.toggle("blue");
 }
 
-//make paragraph dissapear
-document.getElementById("go").onclick = makeInvisible;
+// //make paragraph dissapear
+// document.getElementById("go").onclick = makeInvisible;
+// function makeInvisible() {
+//   document.getElementById("go").style.visibility = "hidden";
+// }
 
-function makeInvisible() {
-  document.getElementById("go").style.visibility = "hidden";
+let dissapear = document.querySelector("#go");
+dissapear.addEventListener("click", hideElement);
+function hideElement() {
+  dissapear.classList.toggle("hidden");
 }
 
-document.getElementById("enter").onmouseenter = changeText;
-function changeText() {
-  document.getElementById("enter").innerHTML = "Hi";
+// document.getElementById("enter").onmouseenter = changeText;
+// function changeText() {
+//   document.getElementById("enter").innerHTML = "Hi";
+// }
+
+let hiMessage = document.querySelector("#enter");
+hiMessage.addEventListener("mouseenter", showHi);
+function showHi() {
+  hiMessage.innerHTML = "Hi";
 }
 
-document.getElementById("enter").onmouseleave = changeTextBack;
-function changeTextBack() {
-  document.getElementById("enter").innerHTML = "Bye";
+let byeMessage = document.querySelector("#enter");
+hiMessage.addEventListener("mouseleave", showBye);
+function showBye() {
+  byeMessage.innerHTML = "Bye";
 }
+
+// document.getElementById("enter").onmouseleave = changeTextBack;
+// function changeTextBack() {
+//   document.getElementById("enter").innerHTML = "Bye";
+// }
 
 //https://www.youtube.com/watch?v=qvypCd2Vl4s
 document.getElementById("clock").onload = showTime();
